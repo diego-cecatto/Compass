0954f91f469934ffe7091d95044276ec23d943db
Diego Cecatto <diego.cecatto@hotmail.com>
2023-10-12 11:24:46 -0300

    fix: implement favicon and build html

--- src/commands/Documentation/Documentation.ts
+++ src/commands/Documentation/Documentation.ts
@@ -9,22 +9,45 @@ dotenv.config();
 
 export class Documentation {
     tsFileDirectory = path.dirname(__filename);
+    outDir = '/build';
 
-    async start() {
+    baseReactFiles() {
+        const sourceDir = this.tsFileDirectory + '../../../../public';
+        const targetDir =
+            this.tsFileDirectory + '../../../../../../' + this.outDir;
+        const files = fs.readdirSync(sourceDir);
+        files.forEach((file) => {
+            const sourceFilePath = path.join(sourceDir, file);
+            const targetFilePath = path.join(targetDir, file);
+            fs.copyFileSync(sourceFilePath, targetFilePath);
+        });
+        var indexFile = targetDir + '/index.html';
+        let indexFileHTML = fs.readFileSync(indexFile, 'utf8');
+        indexFileHTML = indexFileHTML
+            .replace(
+                '</body>',
+                `   <script src="%PUBLIC_URL%/index.js"></script>
+                </body>`
+            )
+            .replace(new RegExp('%PUBLIC_URL%', 'g'), '');
+        fs.writeFileSync(indexFile, indexFileHTML);
+    }
+
+    async build() {
         await this.dependences();
         var indexFile = path.resolve(
             this.tsFileDirectory + '../../../../src/index.tsx'
         );
         const clientEnv = { 'process.env.NODE_ENV': `'production'` };
 
-        esbuild
+        await esbuild
             .build({
                 entryPoints: [indexFile],
                 bundle: true,
                 minify: true,
                 sourcemap: true,
                 define: clientEnv,
-                outdir: './build',
+                outdir: this.outDir,
                 publicPath: '/public',
                 loader: {
                     '.tsx': 'tsx',
@@ -37,7 +60,14 @@ export class Documentation {
                 },
                 jsxFactory: 'React.createElement',
             })
-            .catch(() => process.exit(1));
+            .catch(() => process.exit(1))
+            .then(() => {
+                this.baseReactFiles();
+            });
+    }
+
+    async start() {
+        await this.build();
         const server = new Server();
         server.start();
     }
