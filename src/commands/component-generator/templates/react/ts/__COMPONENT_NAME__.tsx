14d7f1bb08b9f9f50f0c8cce63382d1ae27e78e6
Diego Cecatto <diego.cecatto@hotmail.com>
2023-11-01 21:47:47 -0300

    fix(general): suppress no support for commonjs

---
+++ src/commands/component-generator/templates/react/ts/__COMPONENT_NAME__.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface __COMPONENT_NAME__Props {}
+
+export const __COMPONENT_NAME__: React.FC<__COMPONENT_NAME__Props> = ({}) => {
+    return (
+        <div>
+            <p>__COMPONENT_NAME__ works!</p>
+        </div>
+    );
+};
