486f14313fb61a1726ce70c1837c3e1b00f36f00
Cecatto,Diego <diego_cecatto@dellteam.com>
2023-11-03 19:49:33 -0300

    fix: rename files

---
+++ src/app/pages/component/live-editor/CodePreview.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import * as Dependences from './ComponentDependences';
+
+export const CodePreview = ({
+    component,
+    code,
+}: {
+    component: any;
+    code: any;
+}) => {
+    const getCode = () => {
+        var docCode: string = code.children.trim();
+        var compCode = '';
+        docCode.split('\n').forEach((line) => {
+            if (line.indexOf('import') === -1) {
+                compCode += line;
+            }
+        });
+        return compCode.trim();
+    };
+
+    return (
+        <>
+            <LiveProvider
+                code={getCode()}
+                scope={{
+                    React,
+                    [component.name]: (Dependences as any)[component.name],
+                }}
+                // noInline
+                enableTypeScript
+                language="tsx"
+            >
+                <LiveEditor language="tsx" />
+                <LiveError />
+                <LivePreview language="tsx" />
+            </LiveProvider>
+        </>
+    );
+};
---
+++ src/app/pages/component/live-editor/ComponentDependences.ts
@@ -0,0 +1,3 @@
+export { ClassComponent } from '../../../../examples/ui-scope/class-component/ClassComponent'; 
+export { Breadcrumb } from '../../../../examples/ui-scope/layout/breadcrumb/Breadcrumb'; 
+export { Button } from '../../../../examples/ui-scope/layout/button/Button'; 
