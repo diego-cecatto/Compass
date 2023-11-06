e9a0fc3d4bf186e4f6dfdf60298b055b3b3f87fc
Diego Cecatto <diego.cecatto@hotmail.com>
2023-11-06 08:48:52 -0300

    feature: show hooks into documentation

---
+++ src/services/docgen/hook-resolver.ts
@@ -0,0 +1,51 @@
+import { visitors } from '@babel/traverse';
+//@ts-ignore
+import { NodePath } from 'react-docgen';
+function isHook(path: NodePath, annotation: any) {
+    let inspectPath: NodePath | null = path;
+    do {
+        const name = (inspectPath.node as any)?.id?.name;
+        if (/^use[A-Z].*/.test(name)) {
+            return true;
+        }
+        if (
+            Array.isArray(inspectPath.container) &&
+            !inspectPath.isVariableDeclarator() &&
+            !inspectPath.parentPath?.isCallExpression()
+        ) {
+            return false;
+        }
+    } while ((inspectPath = inspectPath.parentPath));
+    return false;
+}
+function classVisitor(path: any, state: any) {}
+function statelessVisitor(path: any, state: any) {
+    if (isHook(path, state.hooks)) {
+        state.foundDefinitions.add(path);
+    }
+}
+const explodedVisitors = visitors.explode({
+    ArrowFunctionExpression: { enter: statelessVisitor },
+    FunctionDeclaration: { enter: statelessVisitor },
+    FunctionExpression: { enter: statelessVisitor },
+    ObjectMethod: { enter: statelessVisitor },
+    ClassDeclaration: { enter: classVisitor },
+    ClassExpression: { enter: classVisitor },
+});
+
+export class FindHooksDefinitionResolver {
+    hooks;
+    constructor({ hooks = [] } = {}) {
+        this.hooks = hooks;
+    }
+
+    resolve(file: any) {
+        const state = {
+            foundDefinitions: new Set(),
+            hooks: this.hooks,
+        };
+
+        file.traverse(explodedVisitors, state);
+        return Array.from(state.foundDefinitions);
+    }
+}
